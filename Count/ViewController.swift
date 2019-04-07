1b46a8a6761dc259a3ce23d8efef8b4a8aa977c8
sakine218 <snishibayashi218@gmail.com>
2019-04-07 22:36:56 +0900

    first commit

--- Count/ViewController.swift
+++ Count/ViewController.swift
@@ -9,12 +9,60 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var number: Float = 0
+    @IBOutlet var label: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func plus() {
+        number += 1
+        label.text = String(number)
+        
+        colorCheck()
+    }
 
+    @IBAction func minus() {
+        number -= 1
+        label.text = String(number)
+        
+        colorCheck()
+    }
+    
+    @IBAction func times() {
+        number *= 2
+        label.text = String(number)
+        
+        colorCheck()
+    }
+    
+    @IBAction func divided() {
+        number /= 2
+        label.text = String(number)
+        
+        colorCheck()
+    }
+    
+    @IBAction func clear() {
+        number = 0
+        label.text = String(number)
+        
+        colorCheck()
+    }
+    
+    func colorCheck() {
+        if number >= 10 {
+            label.textColor = .red
+        } else if number < 0 {
+            label.textColor = .blue
+        } else {
+            label.textColor = .white
+        }
+    }
 
 }
 
